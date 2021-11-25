3b6c994865ea69f54d66659ddca9a78971cae7c8
Mizael Douglas <mizaeldouglas2@gmail.com>
2021-11-25 17:54:39 -0300

    first commit

---
+++ modelo/assets/js/arquivo.js
@@ -0,0 +1,10 @@
+window.onload = function () 
+{
+    document.querySelector(".menuMobile").addEventListener("click", Function(){
+        if(document.querySelector(".menu nav ul").style.display == 'flex'){
+            document.querySelector(".menu nav ul").style.display = 'none'
+        }else{
+            document.querySelector(".menu nav ul").style.display = 'flex'
+        }
+    })
+}
